583eb9fd540e95deb0a35d413e1eccb3681afe7e
Kirill <budanovkirilldmitrievich@gmail.com>
2021-10-18 17:30:58 +0300

    dz

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['geekshop/tamplates', 'mainapp/tamplates'],
+        'DIRS': ['geekshop/tamplates', 'mainapp/tamplates', 'authapp/tamplates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
